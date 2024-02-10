54d298e1b921f4b0e79acb2361417aa30f52f104
msuengineer//profile <157116228+msuengineer@users.noreply.github.com>
2024-02-10 14:11:06 +0500

    Add files via upload

---
+++ Sudais Usmani/main.py
@@ -0,0 +1,54 @@
+##import random
+##print(random.randrange(3,7))#random is libary in the python and randrange is folder or file in random libry 
+
+# import math
+# print(math.cos(7689765670))
+# print(math.sin(7689765670))
+# print(math.tan(7689765670))
+# print(math.cos(7689765670))
+
+##s="a\nb\tc" #\n mean new line #\t mean spaces equal to tab
+##print(s)
+
+##{
+#how to store the string in ram
+##import random
+##s="spam"
+##s[1]
+##
+###output
+##print(s)
+##}
+
+
+#how to find string 
+##a="Pakistan is great"
+##print(a.find("Pakistan"))
+
+
+#   0    1    2    3  '''/positive indexes/'''
+#   s    p    a    m  '''/the value store in ram/'''
+#  -4   -3   -2   -1  '''/negative indexes/'''
+
+
+##import random
+##p="Computer Programming"
+##print(p)
+
+
+
+
+#   0     1    2    3    4    5    6   '''/positive indexes/'''
+#   c     m    p    u    t    e    r  '''/the value store in ram/'''
+#   -7    -6    -5  -4    -3  -2  -1  '''/negative indexes/'''
+
+import random
+p="Computer Programming"
+p[1]      # use to find the word at which adress [1]=alphabet adress is o   '''/otuput=o/'''
+p[1:9]    # {slicing}        '''/otuput=o/'''
+p[1:9:4]  # 1:9 tell 1 value to nine and 4  {three parameter slicing}    '''/otuput=omputer /'''
+          # describe to add 4 after the out put value    '''/otuput=ot/'''
+# how three slicing work '''/[start:end:diff/direction]/'''
+
+'''/-ve sign -> direction of reading of reading is from R=L/'''
+'''/-ve sign -> direction of reading of reading is from L=R/'''
---
+++ Sudais Usmani/shell 1.py
@@ -0,0 +1,212 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+4
+
+============ RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py ============
+4
+print(s)
+a
+b	c
+import random
+import math
+print(math.sqrt(55))
+7.416198487095663
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+5
+Traceback (most recent call last):
+  File "C:\Users\Admin1\Desktop\Sudais Usmani\main.py", line 26, in <module>
+    print(a.capitilized())
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+3
+Traceback (most recent call last):
+  File "C:\Users\Admin1\Desktop\Sudais Usmani\main.py", line 26, in <module>
+    print(a.capitilized())
+AttributeError: 'str' object has no attribute 'capitilized'. Did you mean: 'capitalize'?
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+5
+Traceback (most recent call last):
+  File "C:\Users\Admin1\Desktop\Sudais Usmani\main.py", line 26, in <module>
+    print(a.capitilized())
+AttributeError: 'str' object has no attribute 'capitilized'. Did you mean: 'capitalize'?
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+6
+-1
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+5
+0
+s="pakistan is great"
+print(s.find("i
+
+
+
+             s")
+             
+SyntaxError: unterminated string literal (detected at line 1)
+
+print(s.find("is"))
+             
+3
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+5
+0
+-1
+ 
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+6
+0
+-1
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+Traceback (most recent call last):
+  File "C:\Users\Admin1\Desktop\Sudais Usmani\main.py", line 35, in <module>
+    print(a.find("computer"))
+NameError: name 'a' is not defined
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+1
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+Computer Programming
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+Computer Programming
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+p="Computer Programming"
+             
+p[1]
+             
+'o'
+p[9]
+             
+'P'
+p[10]
+             
+'r'
+00
+             
+0
+p[3:9]
+             
+'puter '
+p[3:17]
+             
+'puter Programm'
+'puter Programm'
+             
+'puter Programm'
+p[1:9:4]
+             
+'ot'
+
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+
+= RESTART: C:\Users\Admin1\Desktop\Sudais Usmani\main.py
+p[1]
+             
+'o'
+p[1:9]
+             
+'omputer '
+p[1:9:4]
+             
+'ot'
+\
+p[1:9:4]
+             
+'ot'
+P[::-1:]
+             
+SyntaxError: invalid syntax
+P[::-1]
+             
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    P[::-1]
+NameError: name 'P' is not defined. Did you mean: 'p'?
+p="Computer Programming"
+             
+P[::-1]
+             
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    P[::-1]
+NameError: name 'P' is not defined. Did you mean: 'p'?
+p="Computer Programming"
+             
+p[::-1]
+             
+'gnimmargorP retupmoC'
+p="Computer"
+             
+p[::-1]
+             
+'retupmoC'
+p[:2:-1]
+             
+'retup'
+p[:3:-1]
+             
+'retu'
+p[:1:-1]
+             
+'retupm'
+p[1:1:-1]
+             
+''
+p[1:1:-1]
+             
+''
+p[2:1:-1]
+             
+'m'
+p[2:5:-1]
+             
+''
+p[:5:-1]
+             
+'re'
+p[:2:-1]
+             
+'retup'
+p[:3:-1]
+...              
+'retu'
+>>> p[:-2:-1]
+...              
+'r'
+>>> p[-3:-2:-1]
+...              
+''
+>>> p[-3:2:-1]
+...              
+'tup'
+>>> p[-3:3:-1]
+...              
+'tu'
+>>> p[-3:1:-1]
+...              
+'tupm'
+>>> p[3:1:-1]
+...              
+'pm'
+>>> p[5:1:-1]
+...              
+'tupm'
+>>> p[-3:-7:-1]
+...              
+'tupm'
