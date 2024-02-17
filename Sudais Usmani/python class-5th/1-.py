bdb14a85e5fcbf590cbf1bc99a7888929d0f46ba
msuengineer//profile <157116228+msuengineer@users.noreply.github.com>
2024-02-17 13:15:50 +0500

    Add files via upload

---
+++ Sudais Usmani/python class-5th/1-.py
@@ -0,0 +1,9 @@
+a = int(input("Enter The first number :"))
+b = int(input("Enter The second number :"))
+c = int(input("Enter The third number :"))
+if b > a or b > c:
+    print("both condition are true")
+else:
+    print("no output find in our server or condition is false")
+##elif a > b:
+##    print("a is greather than ")
--- Sudais Usmani/python class-5th/1.py
+++ Sudais Usmani/python class-5th/1.py
@@ -3,14 +3,7 @@ b = int(input("Enter The second number :"))
 
 if b > a :
     print("b is greather than a")
-
+elif a > b:
+    print("a is greather than ")
 else:
     print("no output find in our server")
-'''/
-elif a == b:
-    print("a and b are equal")
-elif a > b:
-    print("a is greather than b")
-elif a != b:
-    print("a and b are equal")
-    /'''
