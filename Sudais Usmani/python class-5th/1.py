07cec08645a46237cdacc870be19804bfdf0fe37
msuengineer//profile <157116228+msuengineer@users.noreply.github.com>
2024-02-17 12:14:15 +0500

    Add files via upload

---
+++ Sudais Usmani/python class-5th/1.py
@@ -0,0 +1,16 @@
+a = int(input("Enter The first number :"))
+b = int(input("Enter The second number :"))
+
+if b > a :
+    print("b is greather than a")
+
+else:
+    print("no output find in our server")
+'''/
+elif a == b:
+    print("a and b are equal")
+elif a > b:
+    print("a is greather than b")
+elif a != b:
+    print("a and b are equal")
+    /'''
---
+++ Sudais Usmani/python class-5th/shell1.py
@@ -0,0 +1,31 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+b is greather than a
+
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+b is greather than a
+
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+b is greather than a
+>>> 
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+Enter The first number12
+Enter The second number5446
+b is greather than a
+>>> 
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+Enter The first number :12
+Enter The second number :46
+b is greather than a
+>>> 
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+Enter The first number :45
+Enter The second number :45
+a and b are equal
+>>> 
+= RESTART: C:/Users/Admin1/Desktop/Sudais Usmani/python class-5th/1.py
+Enter The first number :34
+Enter The second number :34
+a and b are equal
