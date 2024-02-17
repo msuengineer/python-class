665759fd7d8c1e74a35a122c4ab18dd7d6a2e05e
msuengineer//profile <157116228+msuengineer@users.noreply.github.com>
2024-02-17 14:33:11 +0500

    Add files via upload

---
+++ Sudais Usmani/python class-5th/range.py
@@ -0,0 +1,14 @@
+'''for i in range(-0,100):
+    print(i,end=",")
+'''
+    
+##fruits=["apple ","banana","cherry"]
+##for x in fruits:
+##    print(x)
+##    if x == "banana":
+##        break
+fruits=["apple ","banana","cherry"]
+for x in fruits:
+    if x == "banana" or "apple":
+        continue
+    print(x)
