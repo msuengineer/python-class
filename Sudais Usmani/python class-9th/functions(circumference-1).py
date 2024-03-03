d0c4527e96e32e9410c8d45583d38f7401e9c0fa
msuengineer//profile <157116228+msuengineer@users.noreply.github.com>
2024-03-03 12:25:43 +0500

    Add files via upload

---
+++ Sudais Usmani/python class-9th/functions(circumference-1).py
@@ -0,0 +1,6 @@
+def circle(r):
+    area=3.14*r**2 # for find area of circle
+    circumference=r*3.14 # for find circumference of circle
+    print("Area of ",r,"is","('", area,"')","\nAnd Circumference of ",r,"is","('",circumference,"')")
+circle(int(input("Enter the Number :")))
+'''this is made by sudais usmani'''
